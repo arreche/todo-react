65faa9b0af89917161afa4a6c2b886476d253c5c
CasiOo <stephan_J@outlook.com>
2015-09-12 23:01:36 +0200

    Removed index.html, removed test from main, DRY http client

--- app/api/httpClient.js
+++ app/api/httpClient.js
@@ -2,49 +2,42 @@ import $ from 'jquery';
 
 export default class HttpClient {
     
-    // GET(`/blabla/$(feafea)/gfeah`)
     GET(url, queryParams) {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                url: url,
-                data: queryParams,
-                success: resolve,
-                error: reject,
-                traditional: true
-            });
+        return _buildRequest({
+            method: 'GET',
+            url: url,
+            data: queryParams
         });
     }
     
     POST(url, data) {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                method : 'POST',
-                url: url,
-                data: data,
-                success: resolve,
-                error: reject,
-                traditional: true
-            });
+        return _buildRequest({
+            method: 'POST',
+            url: url,
+            data: data
         });
     }
     
     PUT(url, data) {
-        return new Promise((resolve, reject) => {
-            $.ajax({
-                method : 'PUT',
-                url: url,
-                data: data,
-                success: resolve,
-                error: reject,
-                traditional: true
-            });
+        return _buildRequest({
+            method: 'PUT',
+            url: url,
+            data: data
         });
     }
     
     DELETE(url, data) {
+        return _buildRequest({
+            method: 'DELETE',
+            url: url,
+            data: data
+        });
+    }
+    
+    _buildRequest(url, method, data) {
         return new Promise((resolve, reject) => {
             $.ajax({
-                method : 'DELETE',
+                method: method,
                 url: url,
                 data: data,
                 success: resolve,
--- app/main.js
+++ app/main.js
@@ -8,12 +8,3 @@ main();
 function main() {
   React.render(<Hello />, document.getElementById('app'));
 }
-
-
-import TodoService from './api/todoService';
-let todoService = new TodoService();
-
-todoService
-    .getAll()
-    .then(result => console.log(result))
-    .catch(err => console.log('Error: ' + err));
